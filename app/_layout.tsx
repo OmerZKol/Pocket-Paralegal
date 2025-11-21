bedc83a774243ef7ebc39fbe5814bc16bc33c115
OmerZKol <theiwrn@gmail.com>
2025-11-21 01:10:10 +0000

    initial commit

---
+++ app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+export default function RootLayout() {
+  return (
+    <>
+      <StatusBar style="light" backgroundColor="#2c3e50" />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </>
+  );
+}
